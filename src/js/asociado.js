41e485790b0ecf4f40fe2e0ff554156470759d14
MirEnovi <garciaivonne11@gmail.com>
2018-12-03 17:18:49 -0600

    add filter function

---
+++ src/js/asociado.js
@@ -0,0 +1,14 @@
+// filtro
+const search = document.getElementById('search');
+const btnSerach = document.getElementById('search-btn');
+
+
+const filter = () => {
+  console.log(search.value);
+  // let newOrder = orders.filter((i) => {
+  //   return orderNumber == search;
+  // })
+  return //newOrder;
+};
+
+btnSerach.addEventListener('click', filter);
