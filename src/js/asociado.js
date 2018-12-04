a02d65b499519094018eeda9bf16afa7ae5c1aa0
MirEnovi <garciaivonne11@gmail.com>
2018-12-04 00:35:33 -0600

    save changes

--- src/js/asociado.js
+++ src/js/asociado.js
@@ -1,19 +1,22 @@
 // filtro
-const search = document.getElementById('search');
-const btnSerach = document.getElementById('search-btn');
+// const search = document.getElementById('search');
+// const btnSerach = document.getElementById('search-btn');
 
 
-const filter = () => {
-  console.log(search.value);
-  // let newOrder = orders.filter((i) => {
-  //   return orderNumber == search;
-  // })
-  return //newOrder;
-};
+// const filter = () => {
+//   console.log(search.value);
+//   // let newOrder = orders.filter((i) => {
+//   //   return orderNumber == search;
+//   // })
+//   return //newOrder;
+// };
 
-btnSerach.addEventListener('click', filter);
+
+// btnSerach.addEventListener('click', filter);
 // Elementos HTML
-const btnLogout = document.getElementById('btnLogout');
+// const btnLogout = document.getElementById('btnLogout');
+
+
 
 // Función para salir
 btnLogout.addEventListener('click', el => {
