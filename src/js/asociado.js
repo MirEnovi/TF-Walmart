966e3b01c69ec48d5b585e6740bf35fde128ce0d
MirEnovi <garciaivonne11@gmail.com>
2018-12-04 16:27:54 -0600

    add change in list menu

--- src/js/asociado.js
+++ src/js/asociado.js
@@ -6,5 +6,6 @@ const btnLogout = document.getElementById('btnLogout');
 
 // Función para salir
 btnLogout.addEventListener('click', el => {
+  // firebase.auth().signOut();
   location.href = ('../views/dashboard.html');
 });
